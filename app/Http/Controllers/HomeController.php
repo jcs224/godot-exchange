9884b5dd20b573e5439331207031882f527f696a
Joe Sweeney <joe.sweeney224@gmail.com>
2015-03-04 23:58:16 -0700

    Initial project setup, added landing page, etc.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller {
 	 */
 	public function __construct()
 	{
-		$this->middleware('auth');
+		//$this->middleware('auth');
 	}
 
 	/**
@@ -30,7 +30,7 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
-		return view('home');
+		return view('layout');
 	}
 
 }
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Godot Exchange Center</title>
+    <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="stylesheets/app.css" />
+    <link rel="stylesheet" href="stylesheets/foundation-icons/foundation-icons.css" />
+    <script src="bower_components/modernizr/modernizr.js"></script>
+</head>
+<body>
+    <div class="contain-to-grid">
+        <nav id="top-bar" class="top-bar" data-topbar role="navigation">
+            <ul class="title-area">
+                <li class="name">
+                    <h1><a href="#">Godot Exchange</a></h1>
+                </li>
+                <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+            </ul>
+
+            <section class="top-bar-section">
+                <ul class="right">
+                    <li><a href="#"><i class="fi-clipboard-pencil"></i> Register</a></li>
+                    <li><a href="#"><i class="fi-arrow-right"></i> Log In</a></li>
+                </ul>
+
+                <ul class="left">
+                    <li><a href="#">Assets</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Learn</a></li>
+                    <li class="has-form">
+                        <div class="row collapse">
+                            <div class="large-8 small-9 columns">
+                                <input type="text">
+                            </div>
+                            <div class="large-4 small-3 columns">
+                                <a id="search-button" href="#" class="button expand">Search</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </section>
+        </nav>
+    </div>
+    
+    <div class="row">
+        <div class="large-12 columns">
+            <div class="row">
+                <div class="show-for-large-up">
+                    <div class="medium-4 columns">
+                    <img src="images/godot.jpg">
+                </div>
+                </div>
+                
+                <div id="banner-column-2-id" class="large-8 medium-12 columns">
+                    <h1>The #1 community resource for the Godot game engine.</h1>
+                    <ul class="feature-list">
+                        <li><i class="fi-check"></i> Engine Plugins</li>
+                        <li><i class="fi-check"></i> Assets (2D/3D art, SFX/music, etc.)</li>
+                        <li><i class="fi-check"></i> Project files</li>
+                        <li><i class="fi-check"></i> Tutorials</li>
+                        <li><i class="fi-check"></i> Latest engine binaries</li>
+                        <li><i class="fi-check"></i> ... and much more! Take a look!</li>
+                    </ul>
+                    <div id="banner-buttons">
+                        <a href="#" class="button large">Browse files</a>
+                        <a href="#" class="button large">Register</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div id="footer">
+        <div class="row">
+            <div class="large-12 columns">
+                <p>&COPY;2015 Godot Exchange. All Rights Reserved.</p>
+            </div>
+        </div>
+    </div>
+    
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
+
+
