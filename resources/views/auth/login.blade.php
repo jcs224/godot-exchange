2ecd8838b7f9905769ad42af8eabdbda9123424d
Joe Sweeney <joe.sweeney224@gmail.com>
2015-03-06 22:09:41 -0700

    Style for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,67 @@
 @extends("layout")
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+<div class="row">
+    <div class="medium-6 medium-offset-3 large-4 large-offset-4 columns">
+        <div class="panel">
+            @if (count($errors) > 0)
+            <div class="errors">
+            <strong>Whoops!</strong>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            
+            <form role="form" method="POST" action="{{ url("auth/login") }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                
+                <div class="row">
+                    <div class="large-12 columns">
+                        <label>Email
+                            <input type="email" name="email" value="{{ old("email") }}">
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="large-12 columns">
+                        <label>Password
+                            <input type="password" name="username">
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="large-12 columns">
+                        
+                        <input id="remember" type="checkbox" name="remember">
+                        <label for="remember">Remember Me</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="large-12 columns">
+                        <input type="submit" id="submit-button" class="button expand" value="Log In">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="large-12 columns">
+                        <a href="{{ url('password/email') }}">Forgot your password?</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-@endsection
+@stop
+
+@section("styles")
+<link rel="stylesheet" href="{{ URL::to("stylesheets/login.css") }}" />
+
+<style>
+    #submit-button {
+        margin-top: 10px;
+    }
+</style>
+@stop
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,25 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Godot Exchange Center</title>
     <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="{{ URL::to("stylesheets/app.css") }}" />
-    <link rel="stylesheet" href="{{ URL::to("stylesheets/foundation-icons/foundation-icons.css") }}" />
+    <link rel="stylesheet" href="{{ url("stylesheets/app.css") }}" />
+    <link rel="stylesheet" href="{{ url("stylesheets/foundation-icons/foundation-icons.css") }}" />
     @yield("styles")
-    <script src="{{ URL::to("bower_components/modernizr/modernizr.js") }}"></script>
+    <script src="{{ url("bower_components/modernizr/modernizr.js") }}"></script>
 </head>
 <body>
     <div class="contain-to-grid">
         <nav id="top-bar" class="top-bar" data-topbar role="navigation">
             <ul class="title-area">
                 <li class="name">
-                    <h1><a href="{{ URL::to("/") }}">Godot Exchange</a></h1>
+                    <h1><a href="{{ url("/") }}">Godot Exchange</a></h1>
                 </li>
                 <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
             </ul>
 
             <section class="top-bar-section">
                 <ul class="right">
-                    <li><a href="{{ URL::to("auth/register") }}"><i class="fi-clipboard-pencil"></i> Register</a></li>
-                    <li><a href="{{ URL::to("auth/login") }}"><i class="fi-arrow-right"></i> Log In</a></li>
+                    <li><a href="{{ url("auth/register") }}"><i class="fi-clipboard-pencil"></i> Register</a></li>
+                    <li><a href="{{ url("auth/login") }}"><i class="fi-arrow-right"></i> Log In</a></li>
                 </ul>
 
                 <ul class="left">
@@ -59,12 +59,10 @@
         </div>
     </div>
     
-    <script src="{{ URL::to("bower_components/jquery/dist/jquery.min.js") }}"></script>
-    <script src="{{ URL::to("bower_components/foundation/js/foundation.min.js") }}"></script>
-    <script src="{{ URL::to("js/app.js") }}"></script>
+    <script src="{{ url("bower_components/jquery/dist/jquery.min.js") }}"></script>
+    <script src="{{ url("bower_components/foundation/js/foundation.min.js") }}"></script>
+    <script src="{{ url("js/app.js") }}"></script>
     
     @yield("scripts")
 </body>
 </html>
-
-
