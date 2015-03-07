2d7b98fb46dc85cb1b4afdf69d05d8520c88f4d8
Joe Sweeney <joe.sweeney224@gmail.com>
2015-03-07 00:11:47 -0700

    Style for forgotten password email page

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,51 +1,54 @@
 @extends("layout")
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Reset Password</div>
-				<div class="panel-body">
-					@if (session('status'))
-						<div class="alert alert-success">
-							{{ session('status') }}
-						</div>
-					@endif
-
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Send Password Reset Link
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+<div class="row">
+    <div class="medium-6 medium-offset-3 large-4 large-offset-4 columns">
+        @if (session('status'))
+            <div data-alert class="alert-box success">
+                {{ session('status') }}
+                <a href="#" class="close">&times;</a>
+            </div>
+        @endif
+        <div class="panel">
+            @if (count($errors) > 0)
+            <div class="errors">
+            <strong>Whoops!</strong>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
+            
+            <form role="form" method="POST" action="{{ url("password/email") }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                
+                <div class="row">
+                    <div class="large-12 columns">
+                        <label>Email
+                            <input type="email" name="email" value="{{ old("email") }}">
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="large-12 columns">
+                        <input type="submit" id="submit-button" class="button expand" value="Sent Password Reset Link">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-@endsection
+@stop
+
+@section("styles")
+<link rel="stylesheet" href="{{ URL::to("stylesheets/login.css") }}" />
+
+<style>
+    #submit-button {
+        margin-bottom: 0;
+    }
+</style>
+@stop
 
