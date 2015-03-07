6708348a1307983d788a1801149f264f3033b8ac
Joe Sweeney <joe.sweeney224@gmail.com>
2015-03-06 19:23:08 -0700

    Basic view structure set up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
-		return view('layout');
+		return view('home');
 	}
 
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -46,29 +46,7 @@
     
     <div class="row">
         <div class="large-12 columns">
-            <div class="row">
-                <div class="show-for-large-up">
-                    <div class="medium-4 columns">
-                    <img src="images/godot_logo.jpg">
-                </div>
-                </div>
-                
-                <div id="banner-column-2-id" class="large-8 medium-12 columns">
-                    <h1>The #1 community resource for the Godot game engine.</h1>
-                    <ul class="feature-list">
-                        <li><i class="fi-check"></i> Engine Plugins</li>
-                        <li><i class="fi-check"></i> Assets (2D/3D art, SFX/music, etc.)</li>
-                        <li><i class="fi-check"></i> Project files</li>
-                        <li><i class="fi-check"></i> Tutorials</li>
-                        <li><i class="fi-check"></i> Latest engine binaries</li>
-                        <li><i class="fi-check"></i> ... and much more! Take a look!</li>
-                    </ul>
-                    <div id="banner-buttons">
-                        <a href="#" class="button large">Browse files</a>
-                        <a href="#" class="button large">Register</a>
-                    </div>
-                </div>
-            </div>
+            @yield("content")
         </div>
     </div>
     
